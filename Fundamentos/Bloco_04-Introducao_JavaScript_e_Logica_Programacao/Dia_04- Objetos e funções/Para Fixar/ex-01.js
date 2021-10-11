d0b066ebd674cac588022710f653f66031267277
severo <oliveira.severo@gmail.com>
2021-10-11 11:04:20 -0300

    Aprendendo maneiras de acessa informacoes dentro de objetos

--- Fundamentos/Bloco_04-Introducao_JavaScript_e_Logica_Programacao/Dia_04- Objetos e funções/Para Fixar/ex-01.js
+++ Fundamentos/Bloco_04-Introducao_JavaScript_e_Logica_Programacao/Dia_04- Objetos e funções/Para Fixar/ex-01.js
@@ -10,3 +10,26 @@ console.log(`${car.type}`);
 
 // Colchetes e tamplate Literals
 console.log(`A marca do meu carro é ${car['type']}`);
+
+// 1 - Crie um objeto player contendo as variáveis listadas abaixo.
+// let name = 'Marta';
+// let lastName = 'Silva';
+// let age = 34;
+// let medals = { golden: 2, silver: 3 };
+let player = {
+    name: 'Marta',
+    lastName: 'Silva',
+    age: 34,
+    medals: { 
+        golden: 2, 
+        silver: 3 
+    }
+};
+
+// para adicionar propriedades 
+player['fullName'] = `${player.name} ${player.lastName}`;
+
+console.log(`O nome completo da jogadora é ${player.fullName}`);
+
+// para acessar um objeto dentro de um objeto 
+console.log(`${player.medals.golden} é o número de medalhas de ouro da jogadora ${player.name}`)
