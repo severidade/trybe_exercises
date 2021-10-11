31319b002e26b99df0a2ef6f402d565752ce1234
severo <oliveira.severo@gmail.com>
2021-10-11 10:44:56 -0300

    Revendo conteudo sobre objetos

--- Fundamentos/Bloco_04-Introducao_JavaScript_e_Logica_Programacao/Dia_04- Objetos e funções/Para Fixar/ex-01.js
+++ Fundamentos/Bloco_04-Introducao_JavaScript_e_Logica_Programacao/Dia_04- Objetos e funções/Para Fixar/ex-01.js
@@ -1,1 +1,12 @@
-fazert ossp 
+
+let car = {
+    type: 'Fiat',
+    model: '500',
+    color: 'white',
+};
+
+// Anotação por ponto e tamplate Literals 
+console.log(`${car.type}`);
+
+// Colchetes e tamplate Literals
+console.log(`A marca do meu carro é ${car['type']}`);
