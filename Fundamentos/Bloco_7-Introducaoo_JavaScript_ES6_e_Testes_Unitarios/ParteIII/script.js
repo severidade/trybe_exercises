30b6496475dbd0600ba1548b028ff581877c926a
severo <oliveira.severo@gmail.com>
2021-10-13 09:57:40 -0300

    Reduzindo uma segunda funcao

--- Fundamentos/Bloco_7-Introducaoo_JavaScript_ES6_e_Testes_Unitarios/ParteIII/script.js
+++ Fundamentos/Bloco_7-Introducaoo_JavaScript_ES6_e_Testes_Unitarios/ParteIII/script.js
@@ -10,7 +10,12 @@ const soma2 = function (num1, num2) {
 }
 console.log(soma2(2,2));
 
-const soma2 = function (num1, num2) {
+// 03. colocando dentro de uam arrow functions
+const soma3 = (num1, num2) => {
     return num1 + num2;
 }
-console.log(soma2(2,2));
+console.log(soma3(2,2));
+
+// 04. Como so termos um retorno podemos colocar em uma linha 
+const soma4 = (num1, num2) => { num1 + num2;}
+console.log(soma3(2,2));
