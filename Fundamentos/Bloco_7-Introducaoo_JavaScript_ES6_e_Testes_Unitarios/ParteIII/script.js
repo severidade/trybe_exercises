dbdf5c2e0061aee7d8d517ae846faa9ab6cefc5b
severo <oliveira.severo@gmail.com>
2021-10-13 10:04:19 -0300

    iniciando revisao arow Functions

--- Fundamentos/Bloco_7-Introducaoo_JavaScript_ES6_e_Testes_Unitarios/ParteIII/script.js
+++ Fundamentos/Bloco_7-Introducaoo_JavaScript_ES6_e_Testes_Unitarios/ParteIII/script.js
@@ -1,8 +1,8 @@
 // 01. Jeito tradicional 
-function soma (num1, num2) {
-    return num1 + num2;
-}
-console.log(soma(2,2));
+// function soma (num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(soma(2,2));
 
 // 02. colocnado dentro de uma veriavel
 const soma2 = function (num1, num2) {
