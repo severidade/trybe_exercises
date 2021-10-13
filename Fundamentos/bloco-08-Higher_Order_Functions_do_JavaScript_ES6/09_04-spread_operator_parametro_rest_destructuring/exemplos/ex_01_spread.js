ec2b9d35cd7511681daef76c7d70bc7e11497084
severo <oliveira.severo@gmail.com>
2021-10-13 11:49:15 -0300

    atualizando o conteúdo do dia 8.4

--- Fundamentos/bloco-08-Higher_Order_Functions_do_JavaScript_ES6/09_04-spread_operator_parametro_rest_destructuring/exemplos/ex_01_spread.js
+++
@@ -1,8 +0,0 @@
-const numbers = [1, 2, 3];
-
-const newArray = [...numbers, 4, 5, 6];
-const myNewArray = [ 5, 8, 5, ...numbers ]
-
-console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
-console.log(numbers); // [ 1, 2, 3 ]
-console.log(myNewArray);
--- Fundamentos/bloco-08-Higher_Order_Functions_do_JavaScript_ES6/09_04-spread_operator_parametro_rest_destructuring/exemplos/ex_03_spread_IMC.js
+++
@@ -1,6 +0,0 @@
-const imc = (peso, altura) => (peso / (altura * altura)).toFixed(2);
-const patientInfo = [60, 1.7];
-
-console.log(imc(...patientInfo)); // 20.76
-// acima e a camada da funcao 
-// a spread 
