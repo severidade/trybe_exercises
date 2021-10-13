2ff0212734a48f7a3ec4240407f7316c9c45ec10
severo <oliveira.severo@gmail.com>
2021-10-13 09:05:30 -0300

    Revendodo conteudo de funcoes

---
+++ Fundamentos/Bloco_7-Introducaoo_JavaScript_ES6_e_Testes_Unitarios/ParteIII/script.js
@@ -0,0 +1,16 @@
+// 01. Jeito tradicional 
+function soma (num1, num2) {
+    return num1 + num2;
+}
+console.log(soma(2,2));
+
+// 02. colocnado dentro de uma veriavel
+const soma2 = function (num1, num2) {
+    return num1 + num2;
+}
+console.log(soma2(2,2));
+
+const soma2 = function (num1, num2) {
+    return num1 + num2;
+}
+console.log(soma2(2,2));
