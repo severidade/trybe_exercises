4995a3551c24bb33680305a97ac6b5790cea1bf7
severo <oliveira.severo@gmail.com>
2021-10-14 15:10:24 -0300

    Exercicio de reducao usnado .join

--- Fundamentos/bloco-08-Higher_Order_Functions_do_JavaScript_ES6/08_04-Reduce/aula_vivo/reduce_array_stings_forma_frase.js
+++ Fundamentos/bloco-08-Higher_Order_Functions_do_JavaScript_ES6/08_04-Reduce/aula_vivo/reduce_array_stings_forma_frase.js
@@ -1,13 +1,15 @@
 const quote = ['Oi', 'é', 'um', 'belíssimo', 'termo'];
 
-const createFrase = (acumulator, currentValue) => {
+const createFrase = (acumulator, currentValue, index) => {
     // console.log(currentValue);
     // return acumulator;
+    // console.log(`${index} || (${acumulator}) é o valor acumulado || ${currentValue}` )
     return `${acumulator} ${currentValue}`;
 }
-const frase = quote.reduce(createFrase, '');
+const frase = quote.reduce(createFrase,'');
 
 // const frase = quote.reduce( ()=> {}, ''); // meu template
 // const frase = quote.reduce((acumulator, currentValue) => { return `${acumulator} ${currentValue}`;}, '');
 
 console.log(frase);
+console.log(quote.join(' '));// como é um array esse forma também é possivel 
