4be69ba1f15b5acf47fb75db26a855413b01782c
severo <oliveira.severo@gmail.com>
2021-10-14 09:40:04 -0300

    Atualizacao

---
+++ Fundamentos/bloco-08-Higher_Order_Functions_do_JavaScript_ES6/08_03-forEach_find_some_every_sort/Para_Fixar/arry_find_can_drive.js
@@ -0,0 +1,19 @@
+// const musicas = [
+//   { id: '31031685', title: 'Partita in C moll BWV 997' },
+//   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+//   { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+// ]
+
+// function findMusic(id) {
+//   // Adicione seu código aqui
+//   return musicas.find((musica) => musica.id === id);
+// }
+
+// console.log(findMusic('31031685'))
+
+const users = [
+  {fristName: 'Marco', lastName: 'Primeiro', canDrive: true },
+  {fristName: 'Jonas', lastName: 'Torres', canDrive: true },
+  {fristName: 'Adélia', lastName: 'Prado', canDrive: false },
+  {fristName: 'Marina', lastName: 'Ribeiro', canDrive: false },
+]
