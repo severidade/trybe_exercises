2d39be8746159fa38715d39c0f8e2e036f581533
severo <oliveira.severo@gmail.com>
2022-01-04 13:52:07 -0300

    Recapiculando o conteudo

--- Front_End/15_gerenciamento-de-estado-com-redux/15_01_o-estado-global-da-aplicacao/script.js
+++ Front_End/15_gerenciamento-de-estado-com-redux/15_01_o-estado-global-da-aplicacao/script.js
@@ -6,17 +6,6 @@ const ESTADO_INICIAL = {
 // 02 ==== Criando o Reducer ====
 const reducer = ( state = ESTADO_INICIAL, action) => {
   switch (action.type){
-    // 03 ==== Actions NEXT_COLOR e PREVIOUS_COLOR  ====
-    case 'NEXT_COLOR':
-      return {
-        ...state,
-        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
-      };
-    case 'PRIVIOUS_COLOR':
-      return {
-        ...state,
-        index: state.index === state.colors.length -1 ? 0 : state.index - 1,
-      };
     default:
       return state;
   }
@@ -26,11 +15,5 @@ const reducer = ( state = ESTADO_INICIAL, action) => {
 
 // 01 ==== Criando a Store ====
 const store = Redux.createStore(reducer);
-
-//04 == Criando eventListeners
-document.getElementById('next').addEventListener('click', () => {
-  store.dispatch({ type: 'NEXT_COLOR' });
-});
-document.getElementById('previous').addEventListener('click', () => {
-  store.dispatch({ type: 'PRIVIOUS_COLOR' });
-});
+// console.log(store);
+// console.log(store.getState());
