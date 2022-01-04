efe6243f5b7002b55d37377d9b880fc0ee18f350
severo <oliveira.severo@gmail.com>
2022-01-04 12:27:13 -0300

    Atualizando os evente listener

--- Front_End/15_gerenciamento-de-estado-com-redux/15_01_o-estado-global-da-aplicacao/script.js
+++ Front_End/15_gerenciamento-de-estado-com-redux/15_01_o-estado-global-da-aplicacao/script.js
@@ -3,6 +3,34 @@ const ESTADO_INICIAL = {
   index: 0,
 };
 
+// 02 ==== Criando o Reducer ====
+const reducer = ( state = ESTADO_INICIAL, action) => {
+  switch (action.type){
+    // 03 ==== Actions NEXT_COLOR e PREVIOUS_COLOR  ====
+    case 'NEXT_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length - 1 ? 0 : state.index + 1,
+      };
+    case 'PRIVIOUS_COLOR':
+      return {
+        ...state,
+        index: state.index === state.colors.length -1 ? 0 : state.index - 1,
+      };
+    default:
+      return state;
+  }
+  console.log(state);
+}
+
 
 // 01 ==== Criando a Store ====
-const store = Redux.createStore();
+const store = Redux.createStore(reducer);
+
+//04 == Criando eventListeners
+document.getElementById('next').addEventListener('click', () => {
+  store.dispatch({ type: 'NEXT_COLOR' });
+});
+document.getElementById('previous').addEventListener('click', () => {
+  store.dispatch({ type: 'PRIVIOUS_COLOR' });
+});
